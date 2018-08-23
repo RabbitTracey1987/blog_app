e80ebf1ed373aed501ed3f0ded3880a87b14e3e4
Ji Wang <traceyw@vt.edu>
2018-08-23 21:36:32 +0800

    hide edit and delte for non-owner or non-signedin users

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -3,15 +3,27 @@ require "rails_helper"
 RSpec.feature "Showing an article" do
 	before do
     @john=User.create!(email:"john@example.com",password:"password")
-    login_as(@john)
+    @ken=User.create!(email:"ken@example.com",password:"password")
     @article = Article.create(title: "First Article", body: "Lorem Ipsum",user: @john) 
   end
-  scenario "A user view the article" do
+  scenario "to non signed in user can not view or edit the article" do
   	visit '/'
   	click_link @article.title
   	expect(page).to have_content(@article.title) 
     expect(page).to have_content(@article.body) 
     expect(current_path).to eq(article_path(@article))
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+  scenario "to non-owner hide view and edit" do
+    login_as(@ken)
+    visit '/'
+    click_link @article.title
+    expect(page).to have_content(@article.title) 
+    expect(page).to have_content(@article.body) 
+    expect(current_path).to eq(article_path(@article))
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
   end
 end
 
