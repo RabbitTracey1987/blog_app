60f61be7cf9f43113e2ed3b5af05522fda7b1018
Ji Wang <traceyw@vt.edu>
2018-08-23 21:37:53 +0800

    show edit and deleto to the signed-in user

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -25,5 +25,15 @@ RSpec.feature "Showing an article" do
     expect(page).not_to have_link("Edit Article")
     expect(page).not_to have_link("Delete Article")
   end
+  scenario "to owner show view and edit" do
+    login_as(@john)
+    visit '/'
+    click_link @article.title
+    expect(page).to have_content(@article.title) 
+    expect(page).to have_content(@article.body) 
+    expect(current_path).to eq(article_path(@article))
+    expect(page).to have_link("Edit Article")
+    expect(page).to have_link("Delete Article")
+  end
 end
 
