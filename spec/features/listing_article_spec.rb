eb540e8456a7b0fb8d119aab96f00695b50fc7eb
Ji Wang <traceyw@vt.edu>
2018-08-23 21:29:33 +0800

    show new article to signed in users

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -3,11 +3,10 @@ require "rails_helper"
 RSpec.feature "listing articles" do 
 	before do	
 		@john=User.create!(email:"john@example.com",password:"password")
-    login_as(@john)
     @article1=Article.create(title: "1st article", body:"Harry Poter",user:@john)
     @article2=Article.create(title: "2nd article", body:"Hello Kitty",user:@john)
 	end
-	scenario "A user lists all articles" do 
+	scenario "with articles created and user not signed in" do 
 		visit "/"
 		expect(page).to have_content(@article1.title)
 		expect(page).to have_content(@article1.body)
@@ -15,6 +14,18 @@ RSpec.feature "listing articles" do
 		expect(page).to have_content(@article2.body)
 		expect(page).to have_link(@article1.title)
 		expect(page).to have_link(@article2.title)
+		expect(page).not_to have_link("New Article")
+	end
+	scenario "with articles created and user signed in" do 
+    login_as(@john)
+		visit "/"
+		expect(page).to have_content(@article1.title)
+		expect(page).to have_content(@article1.body)
+		expect(page).to have_content(@article2.title)
+		expect(page).to have_content(@article2.body)
+		expect(page).to have_link(@article1.title)
+		expect(page).to have_link(@article2.title)
+		expect(page).to have_link("New Article")
 	end
 	scenario "A user has no articles" do 
 		Article.delete_all
